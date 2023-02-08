018d36e778604f796e6aac8f84bce0810c034bd3
rafafrdz <rafafrdz@ebd3f4bd-49fd-0310-bc18-ffb698df80d0>
2023-02-08 19:08:34 +0000

    develop

--- code/scala/redblack-tree/src/main/scala/dev/tfm/redblack/domain/Color.scala
+++
@@ -1,9 +0,0 @@
-package dev.tfm.redblack.domain
-
-sealed trait Color
-case object Red extends Color
-case object Black extends Color
-object Color {
-  type Red = Red.type
-  type Black = Black.type
-}
